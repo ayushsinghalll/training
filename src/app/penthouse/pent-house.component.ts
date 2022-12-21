334db4ba0d4adf72f3d0248e9283f16637a44441
ayush singhal <ayush.dilipsinghal@gmail.com>
2022-12-21 15:44:34 +0530

    Angular day 1

---
+++ src/app/penthouse/pent-house.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:'pent-house',
+   templateUrl:'./penthouse.component.html',
+  styleUrls:['./penthouse.component.css']
+})
+
+export class PentHouseComponent{
+   
+  title='PentHouse Component'
+  name = 'Jalsa'
+
+}
